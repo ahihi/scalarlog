81c5945c951c177625821d20ea6f48ef05e1d13c
Miranda Kastemaa <miranda@foldplop.com>
2016-01-07 05:32:18 +0200

    Get plot data from JSON API rather than HTML

--- static/graph.js
+++ static/graph.js
@@ -24,38 +24,45 @@ function init_canvas(parent, data, options) {
   var chart = new Chart(ctx).Scatter(data, options);
 }
 
-function init_graph(tag, unit, points) {
-  var data = [{
-    label: tag + " (" + unit + ")",
-    pointColor: "rgb(204, 0, 153)",
-    data: points
-  }];
-  var options = {
-    scaleType: "date",
-    scaleDateFormat: "yyyy-mm-dd",
-    scaleTimeFormat: "HH:MM",
-    scaleDateTimeFormat: "yyyy-mm-dd HH:MM",
-    scaleLabel: function(x) {
-      return x.value + " " + unit;
-    },
-    datasetStroke: true,
-    datasetStrokeWidth: 3,
-    datasetStrokeColor: "rgba(204, 0, 153, 0.1)",
-    responsive: true,
-    maintainAspectRatio: false,
-    animation: false
-  };
+function init_graph(json_url) {
+  $.getJSON(json_url, success = function(tag) {
+    var data = [{
+      label: tag.tag + " (" + tag.unit + ")",
+      pointColor: "rgb(204, 0, 153)",
+      data: $.map(tag.data, function(e) {
+        return {
+          x: Date.parse(e.time),
+          y: e.value
+        };
+      })
+    }];
+    var options = {
+      scaleType: "date",
+      scaleDateFormat: "yyyy-mm-dd",
+      scaleTimeFormat: "HH:MM",
+      scaleDateTimeFormat: "yyyy-mm-dd HH:MM",
+      scaleLabel: function(x) {
+        return x.value + " " + tag.unit;
+      },
+      datasetStroke: true,
+      datasetStrokeWidth: 3,
+      datasetStrokeColor: "rgba(204, 0, 153, 0.1)",
+      responsive: true,
+      maintainAspectRatio: false,
+      animation: false
+    };
   
-  var parent = $("#content");
-  var init_canvas_closed = function() { init_canvas(parent, data, options); };
+    var parent = $("#content");
+    var init_canvas_closed = function() { init_canvas(parent, data, options); };
   
-  var resize_timer = null;
-  $(window).resize(function() {
-    if(resize_timer !== null) {
-      clearTimeout(resize_timer);
-    }
-    resize_timer = setTimeout(init_canvas_closed, 50);
-  })
+    var resize_timer = null;
+    $(window).resize(function() {
+      if(resize_timer !== null) {
+        clearTimeout(resize_timer);
+      }
+      resize_timer = setTimeout(init_canvas_closed, 50);
+    })
   
-  init_canvas_closed();
+    init_canvas_closed();
+  });
 }
